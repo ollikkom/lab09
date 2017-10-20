abc7e2ab23659b29b654add8c6ad069a7f55c8b2
ollikkom <olyaamh@mail.ru>
2017-10-21 00:47:47 +0300

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build Status](https://travis-ci.org/ollikkom/lab06.svg?branch=master)](https://travis-ci.org/ollikkom/lab06)
+[![Build Status](https://travis-ci.org/ollikkom/lab07.svg?branch=master)](https://travis-ci.org/ollikkom/lab07)
--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,28 @@
 #include <fstream>
 #include <iostream>
 
+/*!
+	\brief Функция копирования
+	\param[in] text Копировние текстовой строки
+	\param[out] out Вывод на экран 
+	
+	This is the function for output text on screen
+	Код функции:
+	\code
+	void print(const std::string& text, std::ostream& out = std::cout);	
+	\endcode
+*/
 void print(const std::string& text, std::ostream& out = std::cout);
+
+/*!
+        \brief Функция копирования
+        \param[in] text Копировние текстовой строки
+        \param[out] out Вывод на экран  
+
+        This is the function for output text on screen
+  	Код функции:         
+        \code
+        void print(const std::string& text, std::ofstream& out);
+        \endcode
+*/
 void print(const std::string& text, std::ofstream& out);
